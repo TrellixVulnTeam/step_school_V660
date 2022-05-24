5041436b3470d65f9ac422773d67e2f8c29210c2
DrabantBY <drabant2022@gmail.com>
2022-05-24 23:49:56 +0300

    update 24.js

--- js/24.js
+++ js/24.js
@@ -5,11 +5,11 @@ function getCircleLength(r) {
 console.log(getCircleLength(10));
 
 function getNewPrice(price) {
-  return (price * 0.96).toFixed(2); 
+  return (price * 0.965).toFixed(2); 
 }
 console.log(getNewPrice(2.6));
 
 function getMinNum(a, b) {
-  return Math.min(a, b);
+  return a > b ? b : a;
 }
 console.log(getMinNum(10, 7));
