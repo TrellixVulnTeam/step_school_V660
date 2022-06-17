f464088df0e50a4a53fc9854e061c551bc573062
DrabantBY <drabant2022@gmail.com>
2022-06-17 15:42:02 +0300

    add 30.js

---
+++ js/30.js
@@ -0,0 +1,61 @@
+const shape = { color: 'white-red-white', draw() {return `${this.color} ${this.name}`}, };
+
+const rectangle = Object.create(shape, { 
+    name: { value: 'rectangle' }, 
+    sides: { value: {l: 25, h: 10} },
+    points: { value: {x: 0, y: 0, x1: 0, y1: 10, x2: 25, y2: 10, x3: 25, y3: 0} },  
+});
+
+const circle = Object.create(shape, {
+    name: { value: 'circle' }, 
+    raius: { value: 15 }, 
+    point: { value: {x: 0, y: 0} },
+});
+
+const shapes = [rectangle, circle];
+
+shapes.forEach(shape => console.log(shape.draw()));
+
+
+/*******************************************************************************************************/
+
+function Student(firstName, lastName, group, averageMark) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.group = group;
+    this.averageMark = averageMark;
+}
+
+Student.prototype.getScholarship = function() {
+    return this.averageMark >= 5 ? 100 : 80;
+};
+
+
+
+function Aspirant(firstName, lastName, group, averageMark, work) {
+    Student.apply(this, arguments);
+    this.work = work;
+}
+
+Object.setPrototypeOf(Aspirant.prototype, Student.prototype);
+Object.setPrototypeOf(Aspirant, Student);
+
+// Aspirant.prototype = Object.create(Student.prototype);
+// Aspirant.prototype.constructor = Aspirant;
+// Object.assign(Aspirant, Student);
+
+Aspirant.prototype.getScholarship = function() {
+    return this.averageMark >= 5 ? 200 : 180;
+}
+
+const student = new Student('David', 'Jonson', 1, 4);
+const aspirant = new Aspirant('Simon', 'Smith', 2, 7, 'Work')
+const arr = [student, aspirant]
+
+console.log(aspirant.firstName)
+console.log(aspirant.lastName)
+console.log(aspirant.getScholarship())
+
+arr.forEach(person => console.log(person.getScholarship()));
+
+console.log(Aspirant.prototype.constructor === Aspirant)
